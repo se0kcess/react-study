2e9df80af9597dc197e01beb85b652d1a32bfaa2
MinSeok Kim <kimminsuk051@gmail.com>
2024-10-14 12:52:38 +0900

    feat: useReducer, useMemo 실습

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import Average from './components/Average';
+import Counter from './components/Counter';
+import Info from './components/Info';
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+      <Info />
+      <Average />
+    </div>
+  );
+};
+
+export default App;
